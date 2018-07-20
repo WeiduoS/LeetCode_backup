dd6fdbb80501c29602e86098e248528ce8fb0aea
WeiduoS <36429622+WeiduoS@users.noreply.github.com>
2018-07-19 21:56:57 -0400

    Add files via upload

---
+++ LeetCode0236_LowestCommonAncestorOfABinaryTree/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,23 @@
+package LeetCode0236_LowestCommonAncestorOfABinaryTree;
+
+
+public class LowestCommonAncestorOfABinaryTree{
+	public static class TreeNode{
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode (int data){
+			this.val= data;
+		}
+	}
+	
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q)  return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null) return root;
+        return left != null ? left : right;
+    }
+	
+
+}
