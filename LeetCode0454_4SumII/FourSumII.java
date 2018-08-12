a877d3927ae4793471491bae76a4be939f9e2956
WeiduoS <36429622+WeiduoS@users.noreply.github.com>
2018-08-12 17:21:37 -0400

    Add files via upload

---
+++ LeetCode0454_4SumII/FourSumII.java
@@ -0,0 +1,28 @@
+package LeetCode0454_4SumII;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FourSumII {
+	 public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+	        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+	        int res = 0;
+	        for(int i = 0; i < A.length; i++) {
+	            for(int j = 0; j < B.length; j++) {
+	                int sum = A[i] + B[j];
+	                map.put(sum, map.getOrDefault(sum, 0) + 1);
+	            }
+	        }
+	        
+	        for(int i = 0; i < C.length; i++) {
+	            for(int j = 0; j < D.length; j++) {
+	                int sum = C[i] + D[j];
+	                if(map.containsKey(-sum)){
+	                    res = res + map.get(-sum);
+	                }
+	            }
+	        }
+	        
+	        return res;
+	    }
+}
