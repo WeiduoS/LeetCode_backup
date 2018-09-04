dda1e9a6eb067c33092a1c5de97dccb99edca18a
WeiduoS <36429622+WeiduoS@users.noreply.github.com>
2018-09-04 00:31:54 -0400

    Add files via upload

--- LeetCode0428_SerializeAndDeserializeNaryTree/SerializeAndDeserializeNaryTree.java
+++ LeetCode0428_SerializeAndDeserializeNaryTree/SerializeAndDeserializeNaryTree.java
@@ -19,8 +19,14 @@ public class SerializeAndDeserializeNaryTree {
 	};
 	// Encodes a tree to a single string.
     public String serialize(Node root) {
-        return serializeExtend(root, new StringBuilder());
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        String res = serializeExtend(root, sb);
+        res = res + "]";
+        //System.out.println("res: " + res);
+        return res;
     }
+    /*
     private String serializeExtend(Node root, StringBuilder sb) {
         if(root == null) {
             return "";
@@ -29,25 +35,91 @@ public class SerializeAndDeserializeNaryTree {
             for(Node child:root.children) serializeExtend(child,sb);
             sb.append("]");
         }
+        System.out.println("sb: " + sb.toString());
+        return sb.toString();
+    }*/
+    private String serializeExtend(Node root, StringBuilder sb) {
+        if(root == null) {
+            return "";
+        }else{
+            if(root.children.size() > 0) {
+                sb.append("\"" + root.val + "\"" + "[");
+                for(Node child:root.children) serializeExtend(child,sb);
+                sb.append("]");
+            }else{
+                sb.append("\"" + root.val + "\"" + " ");
+            }
+        }
         return sb.toString();
     }
 
     // Decodes your encoded data to tree.
+    /*
     public Node deserialize(String data) {
         int i = 0;
         int len = data.length();
         Node root = null;
         Stack<Node> stack = new Stack<>();
         while(i<len){
-		int start=i;
+		    int start=i;
+            System.out.println("start: " + start + ", i: " + i);
 		    while(i<len && Character.isDigit(data.charAt(i))) i++;
+            System.out.println("i: " + i);
 		        if(start==i){
-			    Node child = stack.pop();
-			    if(stack.isEmpty()) {root=child; break;}
+			        Node child = stack.pop();
+			        if(stack.isEmpty()) {
+                        root=child; break;
+                    }
 			        stack.peek().children.add(child);
-		    }else stack.push(new Node(Integer.parseInt(data.substring(start,i)),new ArrayList<Node>()));
+		    }else{
+                stack.push(new Node(Integer.parseInt(data.substring(start,i)),new ArrayList<Node>()));       
+            }
 		    i++;		
 	    }	
         return root;
+    }*/
+    
+    public Node deserialize(String data) {
+        if(data.length() < 3) return null;
+        Stack<Node> stack = new Stack<>();
+        int len = data.length();
+        int end = 0;
+        Node root = null;
+        for(int i = 0; i < len; i++) {
+            //System.out.println("i: " + i);
+            if(data.charAt(i) == ' ') {
+                continue;
+            }else if(data.charAt(i) == '[') {
+                i += 2;
+                end = parseInt(data, i);
+                //System.out.println("end: " + end);
+                stack.push(new Node(Integer.parseInt(data.substring(i,end)), new ArrayList<Node>()));
+                i = end;
+                //System.out.println(stack.peek().val);
+            }else{
+                
+                Node child = stack.pop();
+                //System.out.println("next val: " + stack.peek().val + ", size: " + stack.size());
+                if(stack.isEmpty()) {
+                    root = child; 
+                    break;
+                }else{
+                    stack.peek().children.add(child);
+                    if(data.charAt(i) == '\"') {
+                        i++;
+                        end = parseInt(data, i);
+                        stack.push(new Node(Integer.parseInt(data.substring(i,end)), new ArrayList<Node>()));
+                        i = end;
+                    }
+                }
+            }
+        }
+        return root;
+    }
+    private int parseInt(String data, int i) {
+        while(data.charAt(i) != '\"') {
+            i++;
+        }
+        return i;
     }
 }
