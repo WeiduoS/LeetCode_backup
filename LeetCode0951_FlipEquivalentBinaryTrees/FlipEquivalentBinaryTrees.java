3f7020cf2542313bfaf513657ff4f37599ed9905
WeiduoS <36429622+WeiduoS@users.noreply.github.com>
2018-12-08 19:24:42 -0500

    Add files via upload

---
+++ LeetCode0951_FlipEquivalentBinaryTrees/FlipEquivalentBinaryTrees.java
@@ -0,0 +1,19 @@
+package LeetCode0951_FlipEquivalentBinaryTrees;
+
+public class FlipEquivalentBinaryTrees {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+	public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null) return true;
+        if(root1 == null || root2 == null) return false;
+        if(root1.val == root2.val) {
+            return (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left)) ||
+            (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right));
+        }
+        return false;
+    }
+}
