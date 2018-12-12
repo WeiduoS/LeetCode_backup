db74b9f172a4f8083739277a88e38bb19b683829
WeiduoS <36429622+WeiduoS@users.noreply.github.com>
2018-12-12 04:26:14 -0500

    Add files via upload

---
+++ LeetCode0804_UniqueMorseCodeWords/UniqueMorseCodeWords.java
@@ -0,0 +1,19 @@
+package LeetCode0804_UniqueMorseCodeWords;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueMorseCodeWords {
+    String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    public int uniqueMorseRepresentations(String[] words) {
+        Set<String> set = new HashSet<>();
+        for(String word : words) {
+            StringBuilder sb = new StringBuilder();
+            for(char c : word.toCharArray()) {
+                sb.append(morse[c - 'a']);
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+}
