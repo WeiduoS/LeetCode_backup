2fa4aef07ecf908d33a98ec31941ab1cee9234ae
WeiduoS <36429622+WeiduoS@users.noreply.github.com>
2018-12-19 07:17:23 -0500

    Add files via upload

---
+++ LeetCode0734_SentenceSimilarity/SentenceSimilarity.java
@@ -0,0 +1,26 @@
+package LeetCode0734_SentenceSimilarity;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class SentenceSimilarity {
+	public boolean areSentencesSimilar(String[] words1, String[] words2, String[][] pairs) {
+        if(words1.length != words2.length) return false;
+        Map<String, Set<String>> map = new HashMap<>();
+        for(String[] pair : pairs) {
+            map.putIfAbsent(pair[0], new HashSet<>());
+            map.putIfAbsent(pair[1], new HashSet<>());
+            map.get(pair[0]).add(pair[1]);
+            map.get(pair[1]).add(pair[0]);
+        }
+        
+        for(int i = 0; i < words1.length; i++) {
+            if(words1[i].equals(words2[i])) continue;
+            if(!map.containsKey(words1[i])) return false;
+            if(!map.get(words1[i]).contains(words2[i])) return false;
+        }
+        return true;
+    }
+}
