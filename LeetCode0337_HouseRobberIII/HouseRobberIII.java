1f9f314ed0a521c0d2242ae9d3e3327101de1bb6
WeiduoS <36429622+WeiduoS@users.noreply.github.com>
2019-01-13 21:52:18 -0500

    Add files via upload

---
+++ LeetCode0337_HouseRobberIII/HouseRobberIII.java
@@ -0,0 +1,66 @@
+package LeetCode0337_HouseRobberIII;
+
+public class HouseRobberIII {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+	/*
+    public int rob(TreeNode root) {
+        if(root == null) return 0;
+        
+        int val = 0;
+        
+        if(root.left != null){
+            val += rob(root.left.left) + rob(root.left.right);
+        }
+        
+        if(root.right != null){
+            val += rob(root.right.left) + rob(root.right.right);
+        }
+        
+        return Math.max(val + root.val, rob(root.left) + rob(root.right));
+    }*/
+    /*
+    public int rob(TreeNode root) {
+        return helper(root, new HashMap<>());
+    }
+    
+    private int helper(TreeNode root, Map<TreeNode, Integer> map) {
+        if(root == null) return 0;
+        if(map.containsKey(root)) return map.get(root);
+        
+        int val = 0;
+        
+        if(root.left != null){
+            val += helper(root.left.left, map) + helper(root.left.right, map);
+        }
+        
+        if(root.right != null){
+            val += helper(root.right.left, map) + helper(root.right.right, map);
+        }
+        
+        val = Math.max(val + root.val, helper(root.left, map) + helper(root.right, map));
+        map.put(root, val);
+        return val;
+    }*/
+    
+    public int rob(TreeNode root) {
+        int[] res = helper(root);
+        return Math.max(res[0], res[1]);
+    }
+    
+    private int[] helper(TreeNode root) {
+        if(root == null) return new int[2];
+        int[] left = helper(root.left);
+        int[] right = helper(root.right);
+        int[] res = new int[2];
+        
+        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        res[1] = root.val + left[0] + right[0];
+    
+        return res;
+    }
+}
