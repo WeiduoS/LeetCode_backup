c0a44f9099fb71f26faaa674da0f5b9833fbcadb
WeiduoS <36429622+WeiduoS@users.noreply.github.com>
2019-01-17 06:38:56 -0500

    Add files via upload

---
+++ LeetCode0972_EqualRationalNumbers/EqualRationalNumbers.java
@@ -0,0 +1,72 @@
+package LeetCode0972_EqualRationalNumbers;
+
+public class EqualRationalNumbers {
+	/*
+    public boolean isRationalEqual(String S, String T) {
+        return helper(S) == helper(T);
+    }
+    private double helper (String s) {
+        int i = s.indexOf('(');
+        if(i > 0) {
+            String base = s.substring(0, i);
+            String rep = s.substring(i + 1, s.length() - 1);
+            for(int j = 0; j < 20; j++) {
+                base += rep;
+            }
+            System.out.println(base);
+            return Double.valueOf(base);
+        }
+        return Double.valueOf(s);
+    }*/
+    public boolean isRationalEqual(String S, String T) {
+        Fraction f1 = convert(S);
+        Fraction f2 = convert(T);
+        return f1.n == f2.n && f1.d == f2.d;
+    }
+    
+    public Fraction convert(String S) {
+        int state = 0;//whole, decimal, repeating
+        Fraction ans = new Fraction(0, 1);
+        int decimal_size = 0;
+        
+        for (String part: S.split("[.()]")) {
+            state++;
+            if (part.isEmpty()) continue;
+            long x = Long.valueOf(part);
+            int sz = part.length();
+
+            if (state == 1) { // whole
+                 ans.iadd(new Fraction(x, 1));
+            } else if (state == 2) { // decimal
+                 ans.iadd(new Fraction(x, (long) Math.pow(10, sz)));
+                 decimal_size = sz;
+            } else { // repeating
+                 long denom = (long) Math.pow(10, decimal_size);
+                 denom *= (long) (Math.pow(10, sz) - 1);
+                 ans.iadd(new Fraction(x, denom));
+            }
+        }
+        return ans;
+    }
+}
+
+class Fraction {
+    long n, d;
+    Fraction (long n, long d) {
+        long g = gcd(n, d);
+        this.n = n / g;
+        this.d = d / g;
+    }
+        
+    public void iadd(Fraction other) {
+        long numerator = this.n * other.d + this.d * other.n;
+        long denominator = this.d * other.d;
+        long g = Fraction.gcd(numerator, denominator);
+        this.n = numerator / g;
+        this.d = denominator / g;
+    }
+        
+    static long gcd(long x, long y) {
+        return x != 0 ? gcd(y % x, x) : y;
+    }
+}
